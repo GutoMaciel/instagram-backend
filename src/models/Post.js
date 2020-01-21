bf64b9e5831c64c4f5c3ed45c3799c4bb31de4e4
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2020-01-21 14:15:03 -0300

    Fix: Post Model at hashtags

--- src/models/Post.js
+++ src/models/Post.js
@@ -4,7 +4,7 @@ const PostSchema = new mongoose.Schema({
   author: String,
   place: String,
   description: String,
-  hashtahs: String,
+  hashtags: String,
   image: String,
   likes: {
     type: Number,
