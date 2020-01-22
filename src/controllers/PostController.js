2f35b4f0512b15bcd335540177c429f3d0ec5092
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2020-01-22 11:12:26 -0300

    Feature: Eslint and others

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,17 +1,19 @@
-const Post = require('../models/Post');
 const sharp = require('sharp');
 const path = require('path');
 const fs = require('fs');
+const Post = require('../models/Post');
 
 module.exports = {
   async index(req, res) {
-    const posts = await Post.find().sort('-createdAt')
+    const posts = await Post.find().sort('-createdAt');
 
     return res.json(posts);
   },
 
   async store(req, res) {
-    const { author, place, description, hashtags } = req.body;
+    const {
+      author, place, description, hashtags,
+    } = req.body;
     const { filename: image } = req.file;
 
     const [name] = image.split('.');
@@ -21,8 +23,8 @@ module.exports = {
       .resize(500)
       .jpeg({ quality: 70 })
       .toFile(
-        path.resolve(req.file.destination, 'resized', fileName)
-      )
+        path.resolve(req.file.destination, 'resized', fileName),
+      );
 
     fs.unlinkSync(req.file.path);
 
@@ -35,7 +37,7 @@ module.exports = {
     });
 
     req.io.emit('post', post);
-    
+
     return res.json(post);
   },
 };
